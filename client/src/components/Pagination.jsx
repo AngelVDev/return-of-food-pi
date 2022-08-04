5df6dfe912abee84d6826039161500bdd0abd91e
Chaos <antonioscutari2@hotmail.com>
2022-08-04 19:41:12 -0300

    [ADD] Pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ foods, foodsPerPage, pagination }) => {
+  let pageNum = [];
+  for (let i = 1; i <= Math.ceil(foods?.length / foodsPerPage); i++) {
+    pageNum.push(i);
+  }
+  if (foods?.length <= 9 || !foods) {
+    return (pageNum = null);
+  }
+  return (
+    <div className="pagesHolder">
+      <ul style={{ margin: "0", display: "contents" }}>
+        {pageNum &&
+          pageNum.map((number) => (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a key={number} className="page" onClick={() => pagination(number)}>
+              {number}
+            </a>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Pagination;
