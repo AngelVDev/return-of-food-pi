8f38c2484abd63f5556c3c02820b35fca8848caf
Chaos <antonioscutari2@hotmail.com>
2022-08-04 19:03:13 -0300

    [CONN] done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import Creator from "./pages/Creator";
+import Details from "./pages/Details";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <Router>
+      <div className="App" style={{ height: "100%" }}>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/details/:id" component={Details} />
+          <Route exact path="/create" component={Creator} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
