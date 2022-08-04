ac377e042c5f0ab11f05b488515aa546d9eba721
Chaos <antonioscutari2@hotmail.com>
2022-08-04 19:39:47 -0300

    [ADD] Landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/pages.css";
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="landingBg">
+      <div className="landingTextDiv">
+        <h1 style={{ fontWeight: "bold" }}>¡Tonio's Kitchen!</h1>
+        <h3 style={{ fontWeight: "lighter" }}>Gourmet and health at home</h3>
+      </div>
+      <button className="landingBtn">
+        <Link
+          style={{
+            textDecoration: "none",
+          }}
+          to="/home"
+        >
+          ¡Andiamo!
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default Landing;
